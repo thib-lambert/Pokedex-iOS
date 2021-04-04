3c0a3f5f02ceacb20d71158ee28efebb79c091b7
Thibaud Lambert <thib.lambert@icloud.com>
2021-04-04 18:46:51 +0200

    [App] Change NavigationBar style

---
+++ Pokedex-iOS/Helpers/Extensions/UIColor+AppColor.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+AppColor.swift
+//  Pokedex-iOS
+//
+//  Created by Thibaud Lambert on 04/04/2021.
+//
+
+extension UIColor {
+    
+    static var navigationControllerTextColor = UIColor(named: "navigationControllerTextColor") ?? .black
+    static var textColor = UIColor(named: "textColor") ?? .white
+}
